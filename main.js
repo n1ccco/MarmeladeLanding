cdf46a71dc7cf565ab1ea896d9403256b29aa3ba
Bohdan Zhuvak <zhuvak.bohdan@gmail.com>
2024-05-29 20:56:56 +0200

    changed icons, added cart, price changes

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
   const products = document.querySelectorAll(".product");
+  const cartQuantitySpan = document.querySelector(".cart-quantity");
+  let totalCartQuantity = 0;
 
   products.forEach((product) => {
     const decreaseButton = product.querySelector(".decrease");
     const increaseButton = product.querySelector(".increase");
+    const addToCartButton = product.querySelector(".price");
     const quantitySpan = product.querySelector(".quantity span");
+    const priceSpan = product.querySelector(".price span");
+    let currentQuantity = parseInt(quantitySpan.textContent);
+    let currentPrice = parseInt(priceSpan.textContent);
 
     decreaseButton.addEventListener("click", function (event) {
       event.preventDefault();
-      let currentQuantity = parseInt(quantitySpan.textContent);
       if (currentQuantity > 0) {
-        quantitySpan.textContent = currentQuantity - 1;
+        currentQuantity -= 1;
+        quantitySpan.textContent = currentQuantity;
+        priceSpan.textContent = currentPrice * currentQuantity;
       }
     });
 
     increaseButton.addEventListener("click", function (event) {
       event.preventDefault();
-      let currentQuantity = parseInt(quantitySpan.textContent);
-      quantitySpan.textContent = currentQuantity + 1;
+      currentQuantity += 1;
+      quantitySpan.textContent = currentQuantity;
+      priceSpan.textContent = currentPrice * currentQuantity;
+    });
+
+    addToCartButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      cartQuantitySpan.style.display = "flex";
+      totalCartQuantity += currentQuantity;
+      cartQuantitySpan.textContent = totalCartQuantity;
     });
   });
 });
